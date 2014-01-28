a8cbb782efea0ea629904efb04d7dd6bbafe505e
GitCat <gitlab@saas.thecmw.cn>
2014-01-29 00:10:42 +0800

    change uploader;

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -3,19 +3,19 @@
 class AttachmentUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  if Rails.env.development?
+  #if Rails.env.development?
     storage :file
 
     def store_dir
       "uploads/#{model.class.to_s.underscore.pluralize}/#{model.id}/#{model.token}"
     end
-  else
-    storage :fog
+  #else
+  #  storage :fog
 
-    def store_dir
-      "#{model.class.to_s.underscore.pluralize}/#{model.id}/#{model.token}"
-    end
-  end
+  #  def store_dir
+  #    "#{model.class.to_s.underscore.pluralize}/#{model.id}/#{model.token}"
+  #  end
+  #end
 
   def extension_white_list
     %w(jpg jpeg gif png)
--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,5 @@
 Recaptcha.configure do |config|
   config.public_key  = APP_CONFIG['recaptcha']['public_key']
   config.private_key = APP_CONFIG['recaptcha']['private_key']
-  config.use_ssl_by_default = true
+  config.use_ssl_by_default = false
 end
